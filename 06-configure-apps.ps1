cbdb074661825d00cc56d0f101dd6c7e90e26e71
natejank <njj3397@rit.edu>
2024-10-20 11:37:18 -0700

    disable web search in the search bar

--- 06-configure-apps.ps1
+++ 06-configure-apps.ps1
@@ -1,1 +1,9 @@
+# configure some settings
+
+# configure terminal to open git bash by default
 Copy-Item -Path .\resources\terminal-settings.json C:\Users\$Env:UserName\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
+
+# disable web search in the search bar
+reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Search /f /v BingSearchEnabled /t REG_DWORD /d 0
+reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Search /f /v AllowSearchToUseLocation /t REG_DWORD /d 0
+reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Search /f /v CortanaConsent /t REG_DWORD /d 0
